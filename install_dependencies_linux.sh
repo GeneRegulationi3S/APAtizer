b3c554322a1a7f4199a01e22a5f89cb7788e06a5
Bruno Sousa <121204829+brss12@users.noreply.github.com>
2024-10-05 23:49:07 +0100

    Update install_dependencies_linux.sh

--- install_dependencies_linux.sh
+++ install_dependencies_linux.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 echo "\nINSTALLING TOOLS (samtools, fastqc, snakemake and htseq-count)...\n"
-sudo apt install samtools fastqc snakemake && pip install HTSeq
+sudo apt install samtools fastqc snakemake python3 && pip install HTSeq
 echo "\nTOOLS INSTALLED.\n"
-
