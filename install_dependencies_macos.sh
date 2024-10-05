12dd651e3084b0f938d1f87327be78a307748e90
Bruno Sousa <121204829+brss12@users.noreply.github.com>
2024-10-05 23:49:28 +0100

    Update install_dependencies_macos.sh

--- install_dependencies_macos.sh
+++ install_dependencies_macos.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 echo "\nINSTALLING TOOLS (samtools, fastqc, snakemake and htseq-count)...\n"
-brew install samtools fastqc snakemake && pip install HTSeq
+brew install samtools fastqc snakemake python3 && pip install HTSeq
 echo "\nTOOLS INSTALLED.\n"
-
